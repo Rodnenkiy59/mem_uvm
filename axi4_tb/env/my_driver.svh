5bcbecb9f907b1db51d056962bb5b68b4068b31d
Nickolay Liamin <nliamin@sitronics.com>
2025-09-25 17:58:42 +0300

    add virtual task

--- axi4_tb/env/my_driver.svh
+++ axi4_tb/env/my_driver.svh
@@ -17,28 +17,16 @@ class my_driver #(type T = uvm_sequence_item) extends uvm_driver #(T);
     if(!uvm_config_db#(virtual axi4_if#(32, 16, 8, 8))::get(this, "", "axi4_if_inst", vif)) begin
       `uvm_error("", "uvm_config_db::get failed")
     end
-  endfunction 
+  endfunction
 
   task run_phase(uvm_phase phase);
-  super.run_phase(phase);
-        
-        // Проверка интерфейса
-        if (vif == null) begin
-            `uvm_fatal("NOVIF", "Virtual interface is null")
-        end
-        
-        @(posedge vif.aclk);
-        forever begin
-            seq_item_port.get_next_item(req);
-            
-            if (req != null) begin
-                `uvm_info("MY_DRIVER", $sformatf("Processed transaction: %s", req.sprint()), UVM_MEDIUM)
-            end else begin
-                `uvm_warning("MY_DRIVER", "Received null transaction")
-            end
-            
-            seq_item_port.item_done();
-        end
+    `uvm_info("MY_DRIVER", "before run_phase", UVM_LOW);
+    super.run_phase(phase);
+    `uvm_info("MY_DRIVER", "after run_phase", UVM_LOW);
+  endtask
+
+  virtual task reset_signals();
+    `uvm_info("MY_DRIVER", "reset_signals", UVM_LOW);
   endtask
 
 endclass: my_driver
@@ -51,13 +39,15 @@ class writeDriver extends my_driver #(my_transaction);
   endfunction //new()
 
   task run_phase(uvm_phase phase);
+    // super.run_phase(phase);
     @(posedge vif.aclk);
+    super.reset_signals();
     forever begin
     @(posedge vif.aclk);
     seq_item_port.get_next_item(req);
     vif.awaddr = req.addr;
     vif.wdata  = req.data;
-    `uvm_info("WRITE_DRIVER", "Transaction sent to DUT", UVM_LOW);
+    // `uvm_info("WRITE_DRIVER", "Transaction sent to DUT", UVM_LOW);
     seq_item_port.item_done();
     end
   endtask
